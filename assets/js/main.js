d757c3ead333d505bd3f17734974bca053d74492
Carlo-Adile <adile.carlo.a@gmailcom>
2024-03-04 17:05:50 +0100

    add markup to dom with every object of array, respective key and value

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,41 @@ const team = [
 ];
 
 /* milestone 1: stampa su console i valori delle key di ogni oggetto */
-for(i = 0; i < team.length; i++){
+/* for(i = 0; i < team.length; i++){
   console.log(team[i]);
+} */
+
+/* milestone 2: stampare le info su DOM sotto forma di stringhe */
+let myDom = document.getElementById('my_dom_print');
+
+/* let tentativo = 'prova a scrivere sul DOM'; */
+
+/* myDom.innerHTML = tentativo; */
+for (i = 0; i < team.length; i++) {
+  /* myDom.innerHTML += team[i]; */
+  const member = team[i];
+  console.log(member)
+
+  const markup =
+  `<div class="col-4">
+    <div class="card m-2 text-center">
+      <div class="card-header">
+        <h4>${member.name}</h4>
+      
+      </div>
+      <div class="card-body">
+        <img src="./assets/img/${member.image}" alt="" class="card-img">
+      </div>
+      <div class="card-footer">
+        <h5>${member.role}</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sapiente itaque tempora!
+        </p>
+        <a href="#" class="btn btn-info">See more</a>
+      </div>
+    </div>
+  </div>`
+
+  myDom.innerHTML += markup;
 }
+
+
